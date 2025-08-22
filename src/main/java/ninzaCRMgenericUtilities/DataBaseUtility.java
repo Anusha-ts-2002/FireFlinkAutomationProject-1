0d81a60ade58d41030227edd28017f6275261ec2
Anusha T S <Anusha T S@FFE-Anusha-TS.mshome.net>
2025-08-22 17:40:13 +0530

    DataBase Utility generic class is commited

---
+++ src/main/java/ninzaCRMgenericUtilities/DataBaseUtility.java
@@ -0,0 +1,13 @@
+package ninzaCRMgenericUtilities;
+
+import org.openqa.selenium.WebDriver;
+
+public class DataBaseUtility {
+
+	WebDriver driver;
+	public void getconnectiontodb(String url,String username,String password)
+	{
+		
+	}
+}
+
